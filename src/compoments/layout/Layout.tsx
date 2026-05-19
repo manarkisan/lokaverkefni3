5d1661b11dbe0f0e2d474925a6a8af606f907ce2
manarkisan <manarkisan@gmail.com>
2026-05-19 12:25:53 +0000

    readme update, tiny navbar work

--- src/compoments/layout/Layout.tsx
+++ src/compoments/layout/Layout.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./Navbar";
+
 export default function Layout() {
     return(
         <>
-        
+        <Navbar></Navbar>
         </>
     )
 }
--- src/compoments/layout/Navbar.tsx
+++ src/compoments/layout/Navbar.tsx
@@ -1,7 +1,9 @@
 export default function Navbar() {
     return(
         <>
-        
+        <header>
+            <h1 className="sticky">navbar</h1>
+        </header>
         </>
     )
 }
