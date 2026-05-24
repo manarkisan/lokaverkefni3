1c3902062cc86a42de5e1a308578a0d68cbfaf64
manarkisan <manarkisan@gmail.com>
2026-05-24 16:27:12 +0000

    product details (as Card) init

---
+++ src/featured/compoments/ProductDetail.tsx
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "#components/ui/card"
+
+
+export default function ProductDetails() {
+   <Card>
+  <CardHeader>
+    <CardTitle>ProductDetails</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+   
+   
+    
+}
