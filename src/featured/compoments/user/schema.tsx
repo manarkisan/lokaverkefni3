405113f23bd114b96986cc4f2dac3a18b06c7456
manarkisan <manarkisan@gmail.com>
2026-06-09 17:49:22 +0000

    email schema fix

--- src/featured/compoments/user/schema.tsx
+++ src/featured/compoments/user/schema.tsx
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const LoginSchema  = z.object({
-    email: z.string().min(5).email().toLowerCase().trim(),
+    email: z.email().toLowerCase().trim(),
     remember_me: z.boolean(),
     password: z.string().trim().min(6, { message: 'Password must be at least 6 characters' } ).max(12, { message: 'Password must be not more than 12 characters' })
 })
@@ -10,7 +10,7 @@ export const LoginSchema  = z.object({
 export type LoginType = z.infer<typeof LoginSchema>
 
 export const SignupSchema  = z.object({
-    email: z.string().min(5).email().toLowerCase().trim(),
+    email: z.email().toLowerCase().trim(),
     remember_me: z.boolean(),
     password: z.string().trim().min(6, { message: 'Password must be at least 6 characters' } ).max(12, { message: 'Password must be not more than 12 characters' })
 })
